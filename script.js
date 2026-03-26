c755d5fdc61de8478e5c9de347d466ea8ae1ed6f
Name <user@example.com>
2026-03-26 23:12:23 +0530

    created Product card and style them hover effect

---
+++ script.js
@@ -0,0 +1,96 @@
+let allProducts = [];
+let filteredProducts = [];
+let cartCount = 0;
+
+// FETCH PRODUCTS
+fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(data => {
+        allProducts = data;
+        filteredProducts = data;
+        displayProducts(data);
+    });
+
+// DISPLAY PRODUCTS
+function displayProducts(products) {
+    const container = document.getElementById("productContainer");
+    container.innerHTML = "";
+
+    products.forEach(p => {
+        const card = `
+        <div class="card">
+            <img src="${p.image}">
+            <h3>${p.title}</h3>
+            <p class="price">₹${(p.price * 80).toFixed(0)}</p>
+            <p class="rating">⭐ ${p.rating.rate}</p>
+            <button class="add-cart" onclick="addToCart()">Add to Cart</button>
+        </div>
+        `;
+        container.innerHTML += card;
+    });
+}
+
+// ADD TO CART
+function addToCart() {
+    cartCount++;
+    document.getElementById("cartCount").innerText = cartCount;
+}
+
+// CATEGORY FILTER
+function filterCategory(category) {
+    if (category === "all") {
+        filteredProducts = allProducts;
+    } else {
+        filteredProducts = allProducts.filter(p => p.category === category);
+    }
+    applyFilters();
+}
+
+// SEARCH
+document.getElementById("search").addEventListener("input", function () {
+    applyFilters();
+});
+
+// PRICE FILTER
+document.getElementById("priceRange").addEventListener("input", function () {
+    document.getElementById("priceValue").innerText = this.value;
+    applyFilters();
+});
+
+// SORT
+document.getElementById("sortPrice").addEventListener("change", applyFilters);
+
+// RATING FILTER
+document.getElementById("ratingFilter").addEventListener("change", applyFilters);
+
+// APPLY ALL FILTERS
+function applyFilters() {
+    let result = [...filteredProducts];
+
+    const searchText = document.getElementById("search").value.toLowerCase();
+    const maxPrice = document.getElementById("priceRange").value;
+    const sortValue = document.getElementById("sortPrice").value;
+    const ratingValue = document.getElementById("ratingFilter").value;
+
+    // SEARCH
+    result = result.filter(p =>
+        p.title.toLowerCase().includes(searchText)
+    );
+
+    // PRICE
+    result = result.filter(p => (p.price * 80) <= maxPrice);
+
+    // RATING
+    if (ratingValue !== "all") {
+        result = result.filter(p => p.rating.rate >= ratingValue);
+    }
+
+    // SORT
+    if (sortValue === "low") {
+        result.sort((a, b) => a.price - b.price);
+    } else if (sortValue === "high") {
+        result.sort((a, b) => b.price - a.price);
+    }
+
+    displayProducts(result);
+}
